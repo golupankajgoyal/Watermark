585a5cd14669b9ae5d6de8b930f993b72ab2b865
golupankajgoyal <golupankajgoyal2@gmail.com>
2021-03-08 22:56:29 +0530

    added activity_enter_code.xml and activity_refer_and_earn.xml files to the layout

---
+++ app/src/main/java/com/example/watermark/DisplayActivity.java
@@ -0,0 +1,14 @@
+package com.example.watermark;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class DisplayActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_refer_and_earn);
+    }
+}
